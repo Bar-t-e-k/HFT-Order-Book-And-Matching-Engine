cabb7cd9c9f7b1650b1c3d224afeee37295a4c71
Bartek <132946708+Bar-t-e-k@users.noreply.github.com>
2026-07-15 14:27:58 +0200

    Implement OrderBook class and enhance main logic for trade processing

--- src/Order.hpp
+++ src/Order.hpp
@@ -3,11 +3,19 @@
 #include <concepts>
 
 template <typename T>
-concept IntegralNumeric = std::integral<T>;
+concept ValidNumericType = std::unsigned_integral<T> && (sizeof(T) >= 4);
 
 enum class Side : uint8_t {
     BUY,
     SELL
+};
+
+struct alignas(64) TradeReport {
+    uint64_t buy_order_id;
+    uint64_t sell_order_id;
+    uint32_t price;
+    uint32_t volume;
+    std::string_view symbol;
 };
 
 struct alignas(64) Order {
@@ -15,11 +23,16 @@ struct alignas(64) Order {
     uint32_t price;
     uint32_t volume;
     Side side;
+    std::string_view symbol;
 
-    void reset(uint64_t new_id, uint32_t new_price, uint32_t new_volume, Side new_side) {
+    Order* next{nullptr};
+
+    void reset(uint64_t new_id, uint32_t new_price, uint32_t new_volume, Side new_side, std::string_view new_symbol) {
         id = new_id;
         price = new_price;
         volume = new_volume;
         side = new_side;
+        symbol = new_symbol;
+        next = nullptr;
     }
 };
